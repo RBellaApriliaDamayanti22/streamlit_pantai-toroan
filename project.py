b3ceb6dc11e536ef5fc2fa94f9b9a8e7b93101b9
RBellaApriliaDamayanti22 <111825442+RBellaApriliaDamayanti22@users.noreply.github.com>
2023-05-28 21:56:26 +0700

    Add files via upload

---
+++ project.py
@@ -0,0 +1,193 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+import altair as alt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
+# import warnings
+# warnings.filterwarnings("ignore")
+
+
+st.title("Kelompok 3")
+st.write("##### R.Bella Aprilia Damayanti (200411100082)")
+st.write("#####  Triasmi Dwi Farawati (200411100186)")
+st.write("##### Informatika Pariwisata A ")
+
+data_set_description, upload_data, preprocessing, modeling, implementation = st.tabs(["Data Set Description", "Upload Data", "Preprocessing", "Modeling", "Implementation"])
+
+with data_set_description:
+    st.write("""# Data Set Description """)
+    st.write("###### Pantai Tlangoh")
+    st.write("###### Pulau Madura merupakan pulau yang menyimpan banyak potensi wisata, yang meliputi keanekaragaman hayati, keindahan bentang alam, potensi wisata alam, peninggalan sejarah atau budaya. Potensi tersebut jika dimanfaatkan untuk kegiatan wisata alam dapat meningkatkan kesejahteraan masyarakat. Potensi wisata pantai Madura, salah satunya terdapat di Kabupaten Sampang. Kabupaten Sampang memiliki potensi sumber daya alam yang sangat menunjang kelangsungan hidup dan pertumbuhan kepariwisataan daerah yang secara kompetitif diharapkan mampu memiliki daya saing dan nilai lebih dari wilayah yang lain. Selain itu, Kabupaten Sampang memiliki beberapa objek wisata alam yang sangat potensial untuk program jangka panjang, seperti Pantai Toroan.")
+    st.write("###### Source Code Aplikasi ada di Github anda bisa acces di link :  https://github.com/RBellaApriliaDamayanti22/streamlit_pantai-toroan")
+
+with upload_data:
+    # uploaded_files = st.file_uploader("Upload file CSV", accept_multiple_files=True)
+    # for uploaded_file in uploaded_files:
+    #     df = pd.read_csv(uploaded_file)
+    #     st.write("Nama File Anda = ", uploaded_file.name)
+    #     st.dataframe(df)
+    df = pd.read_csv('https://raw.githubusercontent.com/RBellaApriliaDamayanti22/streamlit_pantai-toroan/main/Data%20toroan.csv')
+    st.dataframe(df)
+
+with preprocessing:
+    st.subheader("""Normalisasi Data""")
+    st.write("""Rumus Normalisasi Data :""")
+    st.image('https://i.stack.imgur.com/EuitP.png', use_column_width=False, width=250)
+    st.markdown("""
+    Dimana :
+    - X = data yang akan dinormalisasi atau data asli
+    - min = nilai minimum semua data asli
+    - max = nilai maksimum semua data asli
+    """)
+    df = df.drop(columns=["No"])
+    #Mendefinisikan Varible X dan Y
+    X = df.drop(columns=['Grade'])
+    y = df['Grade'].values
+    df
+    X
+    df_min = X.min()
+    df_max = X.max()
+    
+    #NORMALISASI NILAI X
+    scaler = MinMaxScaler()
+    #scaler.fit(features)
+    #scaler.transform(features)
+    scaled = scaler.fit_transform(X)
+    features_names = X.columns.copy()
+    #features_names.remove('label')
+    scaled_features = pd.DataFrame(scaled, columns=features_names)
+
+    st.subheader('Hasil Normalisasi Data')
+    st.write(scaled_features)
+
+    st.subheader('Target Label')
+    dumies = pd.get_dummies(df.Grade).columns.values.tolist()
+    dumies = np.array(dumies)
+
+    labels = pd.DataFrame({
+        '0' : [dumies[0]],
+        '1' : [dumies[1]],
+    })
+
+    st.write(labels)
+
+with modeling:
+    training, test = train_test_split(scaled_features,test_size=0.2, random_state=1)#Nilai X training dan Nilai X testing
+    training_label, test_label = train_test_split(y, test_size=0.2, random_state=1)#Nilai Y training dan Nilai Y testing
+    with st.form("modeling"):
+        st.subheader('Modeling')
+        st.write("Pilihlah model yang akan dilakukan pengecekkan akurasi:")
+        naive = st.checkbox('Gaussian Naive Bayes')
+        submitted = st.form_submit_button("Submit")
+
+        # NB
+        GaussianNB(priors=None)
+
+        # Fitting Naive Bayes Classification to the Training set with linear kernel
+        gaussian = GaussianNB()
+        gaussian = gaussian.fit(training, training_label)
+
+        # Predicting the Test set results
+        y_pred = gaussian.predict(test)
+    
+        y_compare = np.vstack((test_label,y_pred)).T
+        gaussian.predict_proba(test)
+        gaussian_akurasi = round(100 * accuracy_score(test_label, y_pred))
+        # akurasi = 10
+
+        #Gaussian Naive Bayes
+        # gaussian = GaussianNB()
+        # gaussian = gaussian.fit(training, training_label)
+
+        # probas = gaussian.predict_proba(test)
+        # probas = probas[:,1]
+        # probas = probas.round()
+
+        # gaussian_akurasi = round(100 * accuracy_score(test_label,probas))
+
+        #KNN
+        K=10
+        knn=KNeighborsClassifier(n_neighbors=K)
+        knn.fit(training,training_label)
+        knn_predict=knn.predict(test)
+
+        knn_akurasi = round(100 * accuracy_score(test_label,knn_predict))
+
+        #Decission Tree
+        dt = DecisionTreeClassifier()
+        dt.fit(training, training_label)
+        # prediction
+        dt_pred = dt.predict(test)
+        #Accuracy
+        dt_akurasi = round(100 * accuracy_score(test_label,dt_pred))
+
+        if submitted :
+            if naive :
+                st.write('Model Naive Bayes accuracy score: {0:0.2f}'. format(gaussian_akurasi))
+            if k_nn :
+                st.write("Model KNN accuracy score : {0:0.2f}" . format(knn_akurasi))
+            if destree :
+                st.write("Model Decision Tree accuracy score : {0:0.2f}" . format(dt_akurasi))
+        
+        grafik = st.form_submit_button("Grafik akurasi semua model")
+        if grafik:
+            data = pd.DataFrame({
+                'Akurasi' : [gaussian_akurasi, knn_akurasi, dt_akurasi],
+                'Model' : ['Gaussian Naive Bayes', 'K-NN', 'Decission Tree'],
+            })
+
+            chart = (
+                alt.Chart(data)
+                .mark_bar()
+                .encode(
+                    alt.X("Akurasi"),
+                    alt.Y("Model"),
+                    alt.Color("Akurasi"),
+                    alt.Tooltip(["Akurasi", "Model"]),
+                )
+                .interactive()
+            )
+            st.altair_chart(chart,use_container_width=True)
+  
+with implementation:
+    with st.form("my_form"):
+        st.subheader("Implementasi")
+        Weight = st.number_input('Masukkan weight (berat) : ')
+        Lenght = st.number_input('Masukkan lenght (panjang) : ')
+        Circumference = st.number_input('Masukkan Circumference(keliling) : ')
+        model = st.selectbox('Pilihlah model yang akan anda gunakan untuk melakukan prediksi?',
+                ('Gaussian Naive Bayes', 'K-NN', 'Decision Tree'))
+
+        prediksi = st.form_submit_button("Submit")
+        if prediksi:
+            inputs = np.array([
+                Weight,
+                Lenght,
+                Circumference
+            ])
+
+            df_min = X.min()
+            df_max = X.max()
+            input_norm = ((inputs - df_min) / (df_max - df_min))
+            input_norm = np.array(input_norm).reshape(1, -1)
+
+            if model == 'Gaussian Naive Bayes':
+                mod = gaussian
+            if model == 'K-NN':
+                mod = knn 
+            if model == 'Decision Tree':
+                mod = dt
+
+            input_pred = mod.predict(input_norm)
+
+
+            st.subheader('Hasil Prediksi')
+            st.write('Menggunakan Pemodelan :', model)
+
+            st.write(input_pred)
+  
